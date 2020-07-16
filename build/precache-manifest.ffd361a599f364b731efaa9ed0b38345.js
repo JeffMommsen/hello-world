3cb6cbafc987c1c98ad98bb92ac7463835e0b34b
JeffMommsen <36761280+JeffMommsen@users.noreply.github.com>
2020-07-16 16:40:05 +0200

    Initial build

---
+++ build/precache-manifest.ffd361a599f364b731efaa9ed0b38345.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3304a1cc5221134acd9471551b302e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed5d23b0635189269de",
+    "url": "/static/css/main.0abe75dc.chunk.css"
+  },
+  {
+    "revision": "3c397dfd002897908ab3",
+    "url": "/static/js/2.87fd86bd.chunk.js"
+  },
+  {
+    "revision": "64e716c279299c08fdcaa61fec86e6e1",
+    "url": "/static/js/2.87fd86bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed5d23b0635189269de",
+    "url": "/static/js/main.f423e65b.chunk.js"
+  },
+  {
+    "revision": "82554be73c9fd150123b",
+    "url": "/static/js/runtime-main.ee170b54.js"
+  },
+  {
+    "revision": "9518363244f1dabfd95db84ab5b1eef3",
+    "url": "/static/media/homeimg.95183632.png"
+  }
+]);
